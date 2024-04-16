a6a0102523c90d85b06ade0d53f187340b8424db
Zach Herring <2172549+zherring@users.noreply.github.com>
2024-04-16 16:42:44 -0600

    Testing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,20 @@ import { retrieveMessage } from '../utils/ethersUtils';
 import Preview from './components/Preview';
 import Interact from './components/Interact';
 import { contractConfig } from '../config';
+import { Suspense } from 'react';
 
+function InteractFallback() {
+  return <div>Loading...</div>
+}
 
 function App() {
 
   return (
     <div className='flex flex-col md:flex-row'>
       <Preview />
-      <Interact />
+      <Suspense fallback={<InteractFallback />}>
+        <Interact />
+      </Suspense>
     </div>
   )
 }
