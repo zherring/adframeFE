f6e9a6b6fc998d024d55edb50e688952df196e67
Zach Herring <2172549+zherring@users.noreply.github.com>
2024-04-16 16:37:46 -0600

    adding Image + use client

--- src/app/components/Interact.tsx
+++ src/app/components/Interact.tsx
@@ -1,24 +1,25 @@
+'use client'
+
 import React, { useState, useEffect } from 'react';
-// import { useSearchParams } from 'next/navigation'
+import { useSearchParams } from 'next/navigation'
 import SetAd from './InteractSub/SetAd';
 import MintNft from './InteractSub/MintNft';
 import Claim from './InteractSub/Claim';
 
 const Interact: React.FC = () => {
-  // const searchParams = useSearchParams();
+  const searchParams = useSearchParams();
   const [activeTab, setActiveTab] = useState('set');
 
-  // useEffect(() => {
-  //   if (searchParams) {
-  //     // Retrieve the 'tab' parameter from the URL search parameters
-  //     const tab = searchParams.get('tab');
-  //     // Check if the tab parameter is one of the expected values and update the activeTab state
-  //     if (tab && ['set', 'mint', 'claim'].includes(tab)) {
-  //       setActiveTab(tab);
-  //     } else { setActiveTab('set') }
-  //   }
-  // }, [searchParams]); // Depend on searchParams to re-run the effect when query parameters change
-
+  useEffect(() => {
+    if (searchParams) {
+      // Retrieve the 'tab' parameter from the URL search parameters
+      const tab = searchParams.get('tab');
+      // Check if the tab parameter is one of the expected values and update the activeTab state
+      if (tab && ['set', 'mint', 'claim'].includes(tab)) {
+        setActiveTab(tab);
+      } else { setActiveTab('set') }
+    }
+  }, [searchParams]); // Depend on searchParams to re-run the effect when query parameters change
 
   return ( 
     <div className='flex flex-col gap-4 center h-[100vh] md:w-2/3 w-full justify-center p-5'>
--- src/app/components/Preview.tsx
+++ src/app/components/Preview.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import Image from 'next/image';
 import { Copy } from 'react-feather'
 import { useEthereumData } from '../../context/EthereumDataContext'; // Adjust the import path as necessary
 
@@ -16,7 +17,7 @@ import { useEthereumData } from '../../context/EthereumDataContext'; // Adjust t
         <aside>adframe.xyz</aside>
         <h1 className='text-white text-2xl mb-5'>Simple Ads on Farcaster</h1>
         <ol className='text-sm'>
-          <li>1. Launch your adframe contract.</li>
+          <li>1. Launch your adframe.</li>
           <li>2. Drop the adframe into your channel.</li>
           <li>3. Share ad revenue with your channel.</li>  
         </ol>
@@ -25,10 +26,10 @@ import { useEthereumData } from '../../context/EthereumDataContext'; // Adjust t
         {/* <p>How it looks in Farcaster 👇</p> */}
         <div className='flex flex-col items-center mt-5 w-full p-5 rounded-md bg-[#20162A]'>
 
-        <img src="images/example-header.svg" className='w-[340px]' />
+        <Image src="/images/example-header.svg" width={340} height={57} alt="example-header" className='opacity-50' />
         <div className='flex flex-col items-center overflow-hidden'>
           <div className='flex flex-col items-center overflow-hidden rounded-lg bg-[#2a2a32] gap-2'>
-            <img src="api/nft/image" alt="Dynamic Image" className='w-[340px] rounded-sm' />
+            <Image src="/api/nft/image" width={340} height={340} alt="Dynamic Image" className='rounded-sm' />
             <div className='flex flex-row items-center flex-wrap justify-between w-full gap-2 py-1 px-2'>
               {ethereumData.url && (
                 <a href={ethereumData.url} target="_blank" rel="noopener noreferrer" className='min-w-[100px] truncate text-sm bg-[#403A47] flex-1 text-gray-300 text-center py-2 hover:text-gray-100 rounded-md'>
@@ -40,7 +41,7 @@ import { useEthereumData } from '../../context/EthereumDataContext'; // Adjust t
             </div>
           </div>
         </div>
-          <img src="images/example-footer.svg" className='w-[340px]' />
+          <img src="images/example-footer.svg" className='w-[340px] opacity-50' />
         </div>
         {/* <img src="/images/fcaster-example.png" alt="adframe" className='w-[422px]' />
         <img src="api/nft/image" alt="Dynamic Image" className='absolute top-[182px] left-[52px] w-[230px]' /> */}
