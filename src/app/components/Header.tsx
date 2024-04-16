6abd7c478555ccf9689072c68ce985fd591e0470
Zach Herring <2172549+zherring@users.noreply.github.com>
2024-04-16 16:24:00 -0600

    backed eslint version down bc of breaking changes holy cow

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,9 +1,10 @@
 import { ConnectButton } from '@rainbow-me/rainbowkit';
+import Image from 'next/image';
 
 const Header: React.FC = () => {
   return ( 
     <header className='p-2 flex w-full justify-between items-center absolute z-50'>
-      <img className='w-8 invisible' src="images/logo.svg" alt="logo" />
+      <Image width={50} height={50} className='w-8 invisible' src="images/logo.svg" alt="logo" />
       <ConnectButton />
     </header>
   );
