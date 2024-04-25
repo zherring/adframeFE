eb6335f01a2f89b275b9447aafc5a7045f6990ae
Zach Herring <2172549+zherring@users.noreply.github.com>
2024-04-25 14:45:47 -0600

    remove edge

--- src/pages/api/nft/image.tsx
+++ src/pages/api/nft/image.tsx
@@ -1,10 +1,6 @@
 import { ImageResponse } from '@vercel/og';
 import { NextRequest } from 'next/server';
 
-export const config = {
-  runtime: 'edge',
-};
-
 export default async function handler(req: NextRequest) {
 
   const url = new URL(req.url);
